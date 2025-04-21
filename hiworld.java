17716e39235f134e05cd4a6cbf80820801666235
ankit22089 <ankitpanda2208@gmail.com>
2025-04-21 22:26:33 +0530

    New changes

--- hiworld.java
+++ hiworld.java
@@ -1,5 +1,6 @@
 class simple{
     public static void main(String args[]){
 		System.out.println("Hi Ankit ! I hope so dev-ops is good for you.");
+		System.out.println("Hi Batch-36! I hope so you are learning dev-ops actively");
 	}
 }
